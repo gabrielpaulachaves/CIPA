2c1fc9b7a8d2ec35a9ba693b67525c0c157f1b4f
Gabriel <bielpchaves@gmail.com>
2026-05-19 10:38:20 -0300

    continuando projeto

--- trabalho/script.js
+++ trabalho/script.js
@@ -1,7 +1,34 @@
+//npm init -y
+//npm install --save express
+//npm install nodemon -g
+//npm install --save sequelize
+//npm install --save mysql2
+//npm install --save express-handlebars
+//npm install --save body-parser
+
+
+
 const express = require("express")
 const app = express()
 const Sequelize = require("sequelize")
-const sequelize = new Sequelize("CIPA", "root","",{
+const {engine} = require("express-handlebars")
+const bodyparser = require("body-parser")
+const sequelize = new Sequelize("cipa", "root","",{
     host: "localhost",
     dialect: "mysql"
+})
+
+app.engine("handlebars", engine({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json())
+
+app.get("/home", (req, res)=>{
+    res.render("home")
+})
+
+
+
+app.listen(3333, ()=>{
+    console.log("servidor aberto!")
 })
